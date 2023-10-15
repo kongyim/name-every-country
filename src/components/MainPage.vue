536b2d131635140bfc8b01e47eff8937ebb2043c
Kong Yim <kongyim@gmail.com>
2023-10-15 22:20:12 +0100

    ADD: handle reset

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,25 +1,37 @@
 <template>
-  <div class="main-page" v-if="isReady" :style="style" :key="`correct-${correctList.length}`" @click="onClickMap()">
-    <div class="header-box">
+  <div class="main-page" v-if="isReady" @click="onClickMap()">
+    <!-- header -->
+    <div class="header-box" :key="`correct-${correctList.length}`">
       <div class="progress">
         {{correctList.length}} / {{countries.length}}
       </div>
-
       <div class="last-country" v-if="lastCountry">
         <img :src="lastCountry.image" />
         <div>{{lastCountry.country}}</div>
       </div>
+      <div class="give-up" @click="onClickGiveUp" v-if="!isGiveUp">
+        Give up
+      </div>
+      <div class="try-again" @click="onClickTryAgain" v-else>
+        Try again
+      </div>
     </div>
-    <div class="box"
-      v-for="item in countries"
-      :class="{active: item.active}"
-      :key="`item-${item.alpha2}`"
-      :style="item.style"
-      @click="onClickCountryBox(item)"
-      >
+    <!-- map canvas -->
+    <div class="map" key="map" ref="map">
+      <img src="@/assets/map.svg" class="map-background" />
+      <div class="box"
+        v-for="item in countries"
+        :class="{active: item.active, last: item === lastCountry}"
+        :key="`item-${item.alpha2}`"
+        :style="item.style"
+        @click="onClickCountryBox(item)"
+        >
+      </div>
     </div>
+
+    <!-- footer -->
     <div class="input-box">
-      <input ref="input" v-model="inputCountry"  @keyup.enter="onEnter" />
+      <input ref="input" v-model="inputCountry"  @keyup.enter="onEnter" :disable="isGiveUp"/>
     </div>
   </div>
 </template>
@@ -30,25 +42,18 @@ import countries from '../countries'
 
 export default {
   data() {
-    const backgroundWidth = 2520
-    const backgroundHeight = 1260
-
     const mapWidth = 2520
     const mapHeight = 1258
     return {
       countries,
       mapWidth,
       mapHeight,
-      backgroundWidth,
-      backgroundHeight,
-      style: {
-        width: `${backgroundWidth}px`,
-        height: `${backgroundHeight}px`
-      },
       isReady: false,
       inputCountry: '',
       correctList: [],
-      lastCountry: _.first(countries)
+      // lastCountry: _.find(countries, {alpha2: 'VC'})
+      lastCountry: null,
+      isGiveUp: false
     }
   },
   mounted() {
@@ -81,6 +86,7 @@ export default {
         }
         this.lastCountry = find
         const audio = new Audio(find.audio)
+        this.setMapCenter(find)
         audio.play()
       } else {
         const audio = new Audio('./assets/waves/error.mp3')
@@ -91,6 +97,14 @@ export default {
         this.$refs.input.focus()
       })
     },
+    setMapCenter(item) {
+      this.$refs.map.scrollTo({
+        left: item.x - this.$refs.map.clientWidth/2,
+        top: item.y - this.$refs.map.clientHeight/2,
+        behavior: 'smooth',
+      })
+
+    },
     onClickMap() {
       setTimeout(() => {
         this.$refs.input.focus()
@@ -98,6 +112,25 @@ export default {
     },
     onClickCountryBox(item) {
       console.log(item.country, item)
+      this.setMapCenter(item)
+      if (this.isGiveUp) {
+        this.lastCountry = item
+      }
+    },
+    onClickGiveUp() {
+      if (confirm('Are you sure you want to give up?')) {
+        this.isGiveUp = true
+        this.inputCountry = ''
+      }
+    },
+    onClickTryAgain() {
+      this.resetGame()
+    },
+    resetGame() {
+      this.countries = _.map(this.countries, item => _.omit(item, 'active'))
+      this.correctList = []
+      this.lastCountry = ''
+      this.isGiveUp = false
     }
   }
 }
@@ -105,20 +138,34 @@ export default {
 
 <style lang="scss">
 .main-page {
-  background-image: url('../assets/map.svg');
-  .box {
-    width: 9px;
-    height: 9px;
-    margin-left: -4px;
-    margin-top: -4px;
-    background: grey;
-    position: absolute;
-    border: 1px black solid;
-    border-radius: 5px;
-    opacity: 0.8;
-    &.active {
-      background: #00FF00;
-      opacity: 1.0;
+  width: 100vw;
+  height: 100vh;
+  .map {
+    width: 100vw;
+    height: calc(100vh - 57px);
+    // .map-background {
+    //   background-image: url('../assets/map.svg');
+    // }
+    overflow: auto;
+    position: relative;
+    .box {
+      // display: none;
+      width: 9px;
+      height: 9px;
+      margin-left: -4px;
+      margin-top: -4px;
+      background-color: grey;
+      position: absolute;
+      border: 1px black solid;
+      border-radius: 5px;
+      opacity: 0.8;
+      &.active {
+        background-color: #00FF00;
+        opacity: 1.0;
+      }
+      &.last {
+        background-color: yellow;
+      }
     }
   }
   .input-box {
@@ -130,6 +177,7 @@ export default {
       font-size: 20px;
       width: calc(100% - 34px);
       padding: 15px;
+      text-align: center;
     }
   }
   .header-box {
@@ -138,22 +186,40 @@ export default {
     top: 0px;
     font-size: 40px;
     width: 100%;
+    z-index: 100;
     .progress {
       width: 170px;
       text-align: right;
-      color: grey;
+      color: black;
+      padding: 20px;
+      background: rgba(255,255,255,0.8);
     }
     .last-country {
       position: absolute;
-      background: rgba(255,255,255,0.5);
+      background: rgba(255,255,255,0.8);
       padding: 20px;
-      right: 0px;
+      left: 50%;
       top: 0px;
       text-align: center;
-      color: grey;
+      color: black;
+      font-size: 20px;
+      width: 400px;
+      margin-left: -200px;
 
       img {
         width: 100px;
+      }
+    }
+    .give-up, .try-again {
+      position: absolute;
+      background: rgba(255,255,255,0.8);
+      padding: 20px;
+      right: 0px;
+      top: 0px;
+      cursor: pointer;
+      &:hover {
+        background: rgba(0,0,0,0.8);
+        color: white;
       }
     }
   }
